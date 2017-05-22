4f928ca7f5dc5d62565013fca3f1585296e96c7c
Peter Rosario <PeterRosario@ksu.edu>
2017-05-22 14:49:19 -0500

    MainActivity: Added code to verify I am correctly using code in a library.  At this point the project does not work (it compiles but when run it crashes) because I haven't imported or added a depency to javalib.

--- app/src/main/java/org/wheatgenetics/androidlibraryuser/MainActivity.java
+++ app/src/main/java/org/wheatgenetics/androidlibraryuser/MainActivity.java
@@ -4,7 +4,9 @@ package org.wheatgenetics.androidlibraryuser;
  * Uses:
  * android.os.Bundle
  * android.support.v7.app.AppCompatActivity
+ * android.widget.TextView
  *
+ * org.wheatgenetics.androidlibrary.Utils
  * org.wheatgenetics.androidlibraryuser.R
  */
 
@@ -15,5 +17,12 @@ public class MainActivity extends android.support.v7.app.AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         this.setContentView(org.wheatgenetics.androidlibraryuser.R.layout.activity_main);
+
+        final int                     number   = 2;
+        final android.widget.TextView textView = (android.widget.TextView)
+            this.findViewById(org.wheatgenetics.androidlibraryuser.R.id.textView);
+        assert textView != null;
+        textView.setText(java.lang.String.format("doubleOf(%d) is %d",
+            number, org.wheatgenetics.androidlibrary.Utils.doubleOf(number)));
     }
 }
