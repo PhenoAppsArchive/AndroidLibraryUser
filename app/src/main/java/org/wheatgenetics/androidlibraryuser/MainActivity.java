ac7fe575f741a272f8b64676326cd1b220a38b6a
Peter Rosario <PeterRosario@ksu.edu>
2017-05-22 14:10:53 -0500

    Initial revision with just app module.

---
+++ app/src/main/java/org/wheatgenetics/androidlibraryuser/MainActivity.java
@@ -0,0 +1,19 @@
+package org.wheatgenetics.androidlibraryuser;
+
+/**
+ * Uses:
+ * android.os.Bundle
+ * android.support.v7.app.AppCompatActivity
+ *
+ * org.wheatgenetics.androidlibraryuser.R
+ */
+
+public class MainActivity extends android.support.v7.app.AppCompatActivity
+{
+    @java.lang.Override
+    protected void onCreate(final android.os.Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        this.setContentView(org.wheatgenetics.androidlibraryuser.R.layout.activity_main);
+    }
+}
