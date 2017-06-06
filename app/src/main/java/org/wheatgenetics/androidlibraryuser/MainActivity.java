2683b7eca3d5e5ace0242d06ab7422a85e927497
Peter Rosario <PeterRosario@ksu.edu>
2017-06-06 11:07:44 -0500

    MainActivity: Added code to verify I am correctly using code in a foreign module.

--- app/src/main/java/org/wheatgenetics/androidlibraryuser/MainActivity.java
+++ app/src/main/java/org/wheatgenetics/androidlibraryuser/MainActivity.java
@@ -4,8 +4,10 @@ package org.wheatgenetics.androidlibraryuser;
  * Uses:
  * android.os.Bundle
  * android.support.v7.app.AppCompatActivity
+ * android.widget.TextView
  *
  * org.wheatgenetics.androidlibraryuser.R
+ * org.wheatgenetics.javalib.Utils
  */
 
 public class MainActivity extends android.support.v7.app.AppCompatActivity
@@ -15,5 +17,12 @@ public class MainActivity extends android.support.v7.app.AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         this.setContentView(org.wheatgenetics.androidlibraryuser.R.layout.activity_main);
+
+        final int                     number   = 2;
+        final android.widget.TextView textView = (android.widget.TextView)
+            this.findViewById(org.wheatgenetics.androidlibraryuser.R.id.textView);
+        assert null != textView;
+        textView.setText(java.lang.String.format("doubleOf(%d) is %d",
+            number, org.wheatgenetics.javalib.Utils.doubleOf(number)));
     }
 }
